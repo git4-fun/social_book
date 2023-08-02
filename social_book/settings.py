5141a3e40fcbb990bc4b0ee57a686ef439637f8c
Neha Navarkar <66297172+git4-fun@users.noreply.github.com>
2023-08-03 00:30:36 +0530

    changed media root in settings

--- social_book/settings.py
+++ social_book/settings.py
@@ -183,5 +183,8 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
