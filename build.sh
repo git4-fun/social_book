31a6bf3bc8032329bb97e03de139d5e80a3609a1
Neha Navarkar <66297172+git4-fun@users.noreply.github.com>
2023-08-02 22:47:11 +0530

    remove create superuser command

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python3 manage.py createsuperuser --username admin21 --email neha.snavarkar@gmail.com --noinput
