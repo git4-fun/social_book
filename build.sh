e26e2f95f7ecc2ec3cf6296864d137d84a5d77a7
Neha Navarkar <66297172+git4-fun@users.noreply.github.com>
2023-08-02 23:49:53 +0530

    updated superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python3 manage.py createsuperuser --username djadmin --email nehaanavarkar@gmail.com --noinput
