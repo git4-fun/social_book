9b93879ae603517e7b515aa0acd8512d99a46170
Neha Navarkar <66297172+git4-fun@users.noreply.github.com>
2023-08-02 23:28:20 +0530

    removed sourcemap.css.map

--- social_book/settings.py
+++ social_book/settings.py
@@ -160,6 +160,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Following settings only make sense on production and may break development environments.
 # if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
